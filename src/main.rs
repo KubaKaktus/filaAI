5cda37f3c7da1174757e275296192022cdde7a35
KubaKaktus_ <kubavojtisek08@gmail.com>
2024-06-07 11:14:45 +0200

    added stuff

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
-use std::io;    
+use std::io;   
+use rand::Rng;
+
+// enum Hlasky{
+//    Mama,
+//    Tricet5
+// }
+
+fn end(end:String){
+   if end.trim() == "Siberia"{
+      std::process::exit(0);
+   }
+   else{
+      return;
+   }
+}
+
+fn mama(){
+   println!("Tak co chlape, co máma?");
+   let _ =io::stdin().read_line(&mut String::new()).expect("kys nigga");
+   println!("Furt šlape?");
+}
 
 
-fn main() {
+
+fn tricet5(){
    let mut string: String = String::new();
    println!("Řekni 35");
    io::stdin().read_line(&mut string).expect("kys nigga");
-   if string.trim()=="Siberia" {
-      std::process::exit(0)
-   }
-   else{
-      println!("Jdi do píči {string}");
-   }
-   // io::stdin().read_line(&mut String::new()).unwrap();
+   end(string.clone());
+   println!("Jdi do píči {string}");
+}
 
-    main()
+fn main() {
+   let rng: i8 = rand::thread_rng().gen_range(1..=2);
+   match rng {
+      1 => mama(),
+      _ => tricet5(),
+   }
+   let mut input: String = String::new();
+   io::stdin().read_line(&mut input).unwrap();
+
+   if input.trim() == "Siberia"{
+      std::process::exit(0);
+   }
+   else {
+       main();
+   }
+    
 }
