1ac552dbbbbf61f964f7ded2d135d3a7298bfd70
KubaKaktus_ <kubavojtisek08@gmail.com>
2024-06-11 12:06:33 +0200

    added stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::io;   
 use rand::Rng;
+//use colored::Colorize;
 
-// enum Hlasky{
-//    Mama,
-//    Tricet5
-// }
 
+//Ends the program when a Siberia is given
 fn end(end:String){
    if end.trim() == "Siberia"{
       std::process::exit(0);
@@ -15,37 +13,70 @@ fn end(end:String){
    }
 }
 
+
+//Zepta se jestli mama slape
 fn mama(){
    println!("Tak co chlape, co máma?");
-   let _ =io::stdin().read_line(&mut String::new()).expect("kys nigga");
+   let _ =io::stdin().read_line(&mut String::new()).ok(); //ask for input, throwaway
    println!("Furt šlape?");
 }
+
 
 fn tricet5(){
    let mut string: String = String::new();
    println!("Řekni 35");
-   io::stdin().read_line(&mut string).expect("kys nigga");
+   io::stdin().read_line(&mut string).ok();
    end(string.clone());
    println!("Jdi do píči {string}");
 }
 
+fn sibir(){
+   println!("Hej dáš si sibiř?");
+   let mut ask_sibir: String = String::new();
+   io::stdin().read_line(&mut ask_sibir).unwrap();
+   if ask_sibir.trim() == "jo"{
+      println!("Tak to seš borec")
+   }
+   else{
+      println!("Tak to jseš slabá mrdka")
+   }
+
+}
 
 fn main() {
-   let rng: i8 = rand::thread_rng().gen_range(1..=3);
+        clearscreen::clear().expect("failed to clear screen");
+
+        println!(r#"
+        ███████ ██ ██      ██ ██████  
+        ██      ██ ██      ██ ██   ██ 
+        █████   ██ ██      ██ ██████  
+        ██      ██ ██      ██ ██      
+        ██      ██ ███████ ██ ██      
+                                      
+                                      
+            "#);
+                       
+   let rng: i8 = rand::thread_rng().gen_range(1..=12);
    match rng {
       1 => mama(),
-      2 => println!("Ti takovou napalim že us se nopoasdíš"),
+      2 => println!("Ti takovou napalim že us se nenapiješ"),
+      3 => println!("Hmm."),
+      4 => println!("Ty seš fakt charakter."),
+      5 => println!("To je hafo dobrý"),
+      6 => println!("Ty mrrrdko"),
+      7 => println!("Ty zmrde"),
+      8 => println!("Ty jsi píči a už se nenapiješ."),
+      9 => println!("Ty jsi píči"),
+      10 => println!("Nabí mi"),
+      11 => sibir(),
       _ => tricet5(),
    }
 
    let mut input: String = String::new();
    io::stdin().read_line(&mut input).unwrap();
+   end(input);
+   main()
 
-   if input.trim() == "Siberia"{
-      std::process::exit(0);
-   }
-   else {
-       main();
-   }
+   
     
 }
